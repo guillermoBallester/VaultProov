78f5b807ae7d8c5a69f7f0a48ade99f5e1c71c20
guillermoBallester <guillermo.ballester@coop.no>
2026-03-02 18:11:01 +0100

    Add basic CLI argument handling mechanism

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting VaultProov...");
+
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        1 => println!("No arguments provided"),
+        2 => println!("Argument missing, format should be: --verify file.xx"),
+        3 => println!("arguments {}, {}", args[1], args[2]),
+        _ => println!("Too many arguments"),
+    }
 }
