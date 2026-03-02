2dc928a5f3df19da8ebc70cb76159a418f6f47ac
guillermoBallester <guillermo.ballester@coop.no>
2026-03-02 18:19:26 +0100

    Enhance CLI argument handling with validation and graceful exits

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
 use std::env;
+use std::process;
 
 fn main() {
     println!("Starting VaultProov...");
 
     let args: Vec<String> = env::args().collect();
     match args.len() {
-        1 => println!("No arguments provided"),
-        2 => println!("Argument missing, format should be: --verify file.xx"),
-        3 => println!("arguments {}, {}", args[1], args[2]),
-        _ => println!("Too many arguments"),
+        1 => {
+            println!("No arguments provided");
+            process::exit(1)
+        }
+        2 => {
+            println!("Argument missing, format should be: verify file.xx");
+            process::exit(1)
+        }
+        3 => {
+            if args[1] == "verify" {
+                println!("Verifying file: {}", args[2])
+            } else {
+                println!("Invalid argument: {}", args[1]);
+                process::exit(1)
+            }
+        }
+        _ => {
+            println!("Too many arguments");
+            process::exit(1)
+        }
     }
 }
