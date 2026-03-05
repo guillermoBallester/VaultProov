caa01b8fed92d607083727d879245c39e485c2db
guillermoBallester <guillermo.ballester@coop.no>
2026-03-05 10:11:40 +0100

    Add `VaultProovError` enum for error handling and improve CLI argument validation

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
+mod error;
+
 use std::env;
-use std::process;
 use c2pa::Reader;
+use error::VaultProovError;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), VaultProovError> {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 || args[1] != "verify" {
-        eprintln!("Usage: vaultproov verify <file>");
-        process::exit(1);
+    if args.len() < 2 {
+        return Err(VaultProovError::Other("missing arguments".to_string()))
     }
-
-    let filename = &args[2];
-    let reader = Reader::from_file(filename)?;
-    println!("{}", reader.json());
-    Ok(())
+    match args[1].as_str() {
+        "verify" => {
+            if args.len() != 3 {
+                return Err(VaultProovError::Other("missing file name".to_string()))
+            }
+            let filename = &args[2];
+            let reader = Reader::from_file(filename)?;
+            println!("{}", reader.json());
+            Ok(())
+        },
+        _ => Err(VaultProovError::Other("Invalid arguments".to_string()))
+    }
 }
