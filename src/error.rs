6e55ff8ee3af85d6a6bc2f46aa712ed4dc05eb0b
guillermoBallester <guillermo.ballester@coop.no>
2026-03-06 13:48:14 +0100

    Add multipart file upload support for `/verify` endpoint

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -6,7 +8,20 @@ pub enum VaultProovError {
     IoError(#[from] std::io::Error),
     #[error("C2PA/error {0}")]
     C2PAError(#[from] c2pa::Error),
+    #[error("multipart error {0}")]
+    MultipartError(#[from] axum::extract::multipart::MultipartError),
     #[error("{0}")]
     Other(String),
+}
+
+impl IntoResponse for VaultProovError {
+    fn into_response(self) -> Response {
+        match self {
+            VaultProovError::IoError(_) => (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()).into_response(),
+            VaultProovError::C2PAError(_) => (StatusCode::BAD_REQUEST, self.to_string()).into_response(),
+            VaultProovError::MultipartError(_) => (StatusCode::BAD_REQUEST, self.to_string()).into_response(),
+            VaultProovError::Other(_) => (StatusCode::BAD_REQUEST, self.to_string()).into_response(),
+        }
+    }
 }
 
